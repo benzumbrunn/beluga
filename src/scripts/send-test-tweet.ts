80e0407777a4c7cf5feea239afbb73797afa4b5f
Ben Zumbrunn <ben@cakedefi.com>
2021-07-18 23:07:40 +0800

    Improve emoji test tweet script

--- src/scripts/send-test-tweet.ts
+++ src/scripts/send-test-tweet.ts
@@ -1,10 +1,26 @@
-import { SHRIMP, FISH, DOLPHIN, SHARK, WHALE } from "../twitter/getEmojiFromValue";
+import BigNumber from "bignumber.js";
+import { formatTweet } from "../twitter/formatTweet";
 import { tweet } from "../twitter/tweet";
+import { Swap } from "../types/Swap";
+
+const swap: Swap = {
+  baseTokenAmount: 100,
+  quoteTokenAmount: 1000000,
+  _id: '123',
+  blockHeight: 1234,
+  blockTime: 'test time',
+  txid: '349853453450',
+  txType: 'Swap',
+  poolId: '5',
+  baseTokenSymbol: 'TEST',
+  quoteTokenSymbol: 'DFI',
+  __v: 0,
+};
 
 const sendTestTweet = async () => {
-  const msg = SHRIMP + FISH + DOLPHIN + SHARK + WHALE;
-  const tweetId = await tweet(msg);
-  console.log(msg);
+  const formattedTestTweet = formatTweet(swap, new BigNumber(1000000));
+  const tweetId = await tweet(formattedTestTweet);
+  console.log(formattedTestTweet);
   console.log(tweetId);
 }
 
