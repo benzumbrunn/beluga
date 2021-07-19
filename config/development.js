fe20bfb316a8f107bae7398bdf21377b50ed9baa
Ben Zumbrunn <ben@cakedefi.com>
2021-07-19 20:57:10 +0800

    Improve error handling and development config

---
+++ config/development.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+module.exports = {
+  usdThreshold: 10,
+  watchIntervalInSeconds: 5,
+}
--- src/query/dfcApi.ts
+++ src/query/dfcApi.ts
@@ -2,7 +2,13 @@ import axios from "axios";
 import { Swap } from "../types/Swap";
 
 const getLatestSwapTransactions = async (id: string, limit = '100'): Promise<Swap[]> => {
-  return (await axios.get(`https://api.defichain.io/v1/getswaptransaction?id=${id}&network=mainnet&skip=0&limit=${limit}`)).data.data;
+  try {
+    const response = await axios.get(`https://api.defichain.io/v1/getswaptransaction?id=${id}&network=mainnet&skip=0&limit=${limit}`);
+    return response.data.data
+  } catch (err) {
+    console.error(err.response.statusText);
+    throw new Error(err.response.statusText);
+  }
 }
 
 export {
