8e6f6b4820047386894e31f906aff98b09cc36ab
Ben Zumbrunn <ben@cakedefi.com>
2021-07-20 15:45:13 +0800

    Move to json based configs

--- config/development.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-undef */
-module.exports = {
-  usdThreshold: 10,
-  watchIntervalInSeconds: 5,
-}
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  verbose: true,
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/dist/"
-  ],
-};
-
-module.exports = config;
