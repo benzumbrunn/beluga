15bd68db11850eee039ff36900aafae02c2cb9b6
JJ Adonis <josejaimecareers@gmail.com>
2021-10-20 16:17:56 +0800

    updates txid to explorer link

--- src/twitter/formatTweet.ts
+++ src/twitter/formatTweet.ts
@@ -6,7 +6,7 @@ const formatTweet = (swap: Swap, usdValue: BigNumber): string => {
   const emoji = getEmojiFromValue(usdValue);
   const infoMsg = `${new BigNumber(swap.baseTokenAmount).toFormat()} #${swap.baseTokenSymbol} swapped to ${new BigNumber(swap.quoteTokenAmount).toFormat()} #${swap.quoteTokenSymbol}`;
   const valueMsg = `\n${emoji} Value: ${usdValue.toFormat()} USD`;
-  const txidMsg = `\n\n${swap.txid}`
+  const txidMsg = `\n\nhttps://defiscan.live/transactions/${swap.txid}`
   return infoMsg.concat(valueMsg).concat(txidMsg);
 }
 
