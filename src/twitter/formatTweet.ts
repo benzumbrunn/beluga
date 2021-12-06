2fc81e2233a815ae02da528b7d4ed00fadca0f75
Ben Zumbrunn <ben@cakedefi.com>
2021-12-06 21:03:38 +0800

    Use $ symbol for currencies in tweet

--- src/twitter/formatTweet.ts
+++ src/twitter/formatTweet.ts
@@ -4,7 +4,7 @@ import { getEmojiFromValue } from "./getEmojiFromValue";
 
 const formatTweet = (swap: Swap, usdValue: BigNumber): string => {
   const emoji = getEmojiFromValue(usdValue);
-  const infoMsg = `${new BigNumber(swap.baseTokenAmount).toFormat()} #${swap.baseTokenSymbol} swapped to ${new BigNumber(swap.quoteTokenAmount).toFormat()} #${swap.quoteTokenSymbol}`;
+  const infoMsg = `${new BigNumber(swap.baseTokenAmount).toFormat()} $${swap.baseTokenSymbol} swapped to ${new BigNumber(swap.quoteTokenAmount).toFormat()} $${swap.quoteTokenSymbol}`;
   const valueMsg = `\n${emoji} Value: ${usdValue.toFormat()} USD`;
   const txidMsg = `\n\n${swap.txid}`
   return infoMsg.concat(valueMsg).concat(txidMsg);
